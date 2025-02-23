dc82614e0a004440397ed7fd2aed8e3d02781ffb
gdysidik <gundik178@gmail.com>
2025-02-23 14:49:50 +0700

    test

--- soal1.c
+++ soal1.c
@@ -3,17 +3,17 @@
 #include <stdio.h>
 
 int main() {
-    floats alas, tinggi, luas;
+    float alas, tinggi, luas;
 
     printf("Masukkan alas segitiga: ");
     scanf("%f", &alas);
     
     printf("Masukkan tinggi segitiga: ");
-    scanf("%f", #tinggi);
+    scanf("%f", &tinggi);
 
     luas = 0.5 * alas * tinggi;
 
-    printf("Luas segitiga adalah: %.2f\n, luas");
+    printf("Luas segitiga adalah: %.2f\n", luas);
     
     0;
 }
