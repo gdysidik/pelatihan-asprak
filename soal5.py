b7fc9d3c00ebe57b433d94d08c029ed9582507f5
gdysidik <145112956+gdysidik@users.noreply.github.com>
2025-02-25 00:46:58 +0700

    soal5 bener kynya

--- soal5.py
+++ soal5.py
@@ -3,15 +3,29 @@ def has_duplicate_dictionary(lst):
     # item yang terdapat di list dan value adalah seberapa
     # sering item tersebut muncul di list
     frequency = {}
+    CNT = 1
+    cek = False
     for item in lst:
-        pass
+        if item in frequency:
+            frequency.update({item: CNT+1})
+            CNT+=1
+            cek = True
+        else:
+            frequency.update({item: 1})
+    if cek:
+        print("has duplicate item")
+    else:
+        print("no duplicate item")
+    return frequency
 
 
 def has_duplicate_set(lst):
     # set ini berisi item yang sudah pernah dilihat dalam lst
     seen = set()
+    cek = False
     for item in lst:
-        pass
+        seen.add(item)
+    return seen
 
 
 def main():
